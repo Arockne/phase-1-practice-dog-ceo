3def7788c49bdb01144cfdf9ec01f73cd4fefe57
Daniel Calvin <arockne2003@yahoo.com>
2021-07-31 09:24:10 -0700

    Implement getRandomImages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('%c HI', 'color: firebrick')
+
+//on page load, fetches the images using "https://dog.ceo/api/breeds/image/random/4"
+//parses the response as json
+//add image elements to the DOM for each image in the array
+function getRandomImages() {
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(resp => resp.json())
+  .then(json => createImages(json.message))
+  .catch(error => document.body.appendChild(error.message))
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  getRandomImages();
+})
