27aac6347729c5ccb841d5548e60a7642adbe158
Daniel Calvin <arockne2003@yahoo.com>
2021-07-31 11:50:54 -0700

    Refactor majority of functions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 console.log('%c HI', 'color: firebrick')
 
+function getRandomImages() {
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(resp => resp.json())
+  .then(appendImages)
+  .catch(error => document.querySelector().appendChild(error.message))
+}
+
+function appendImages(json) {
+  const images = json.message;
+  images.forEach(image => {
+    const img = document.createElement('img');
+    img.src = image;
+    img.width = 350;
+    document.querySelector('#dog-image-container').appendChild(img);
+  })
+}
+
+function getDogBreeds() {
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(resp => resp.json())
+  .then(json => json.message)
+}
+
+function init() {
+  getRandomImages();
+}
+
+init();
